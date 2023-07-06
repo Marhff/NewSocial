3d8c6c25c95997d292bcb9d9382e10f89570fe6d
Marcos França <99540448+Marhff@users.noreply.github.com>
2023-07-06 13:24:50 -0300

    Config initial

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  './Footer.scss' 
+
+const Footer = () => { 
+  return (
+    <div className='styles.Footer'>Footer</div>
+  )
+}
+import styles from './Header.scss' 
+
+export default Footer
